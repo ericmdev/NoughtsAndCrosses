f9d33ca154e7514bdccc8b499b350ef57a1d50d1
Eric Mugerwa <ericmdev@gmail.com>
2016-01-10 00:32:57 +0000

    Add src/ApplicationInterface.php

---
+++ src/ApplicationInterface.php
@@ -0,0 +1,16 @@
+<?php
+namespace NoughtsAndCrosses;
+use NoughtsAndCrosses\Config\ConfigInterface;
+use NoughtsAndCrosses\Game\GameInterface;
+
+/** 
+ * Interface for Application.
+ *
+ * @package   noughtsandcrosses
+ */
+interface ApplicationInterface
+{
+    public function setConfig(ConfigInterface $config);
+    public function setGame(GameInterface $game);
+    public function run();
+}
