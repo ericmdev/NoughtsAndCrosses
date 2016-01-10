c87c55b376617e7a4d6618775ab2cc0d1766f832
Eric Mugerwa <ericmdev@gmail.com>
2016-01-10 21:39:48 +0000

    Add class interface

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -10,5 +10,38 @@ use Exception;
  */
 class Config implements ConfigInterface
 {
-    
+    /**
+     * @access protected
+     * @var    stdClass
+     */
+    protected  $game;
+
+    /**
+     * Constructor.
+     * 
+     */
+    public function __construct()
+    {   
+    }
+
+    /**
+     * Get Game.
+     * 
+     * @return stdClass
+     */
+    public function getGame()
+    {
+        
+    }
+
+    /**
+     * Set Game.
+     * 
+     * @param  array    $game Game configuration data.
+     * @return stdClass
+     */
+    public function setGame(array $data)
+    {
+
+    }
 }
