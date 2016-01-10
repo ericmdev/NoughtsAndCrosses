563f9f997601f14284208f66e675c0aa96071606
Eric Mugerwa <ericmdev@gmail.com>
2016-01-10 23:30:46 +0000

    Add src/Game/Player/Player.php

---
+++ src/Game/Player/Player.php
@@ -0,0 +1,19 @@
+<?php
+namespace NoughtsAndCrosses\Game\Player;
+
+/**
+ * Player
+ *
+ * @package noughtsandcrosses
+ */
+class Player implements PlayerInterface
+{
+    /**
+     * Constructor.
+     * 
+     */
+    public function __construct()
+    {
+        
+    }
+}
