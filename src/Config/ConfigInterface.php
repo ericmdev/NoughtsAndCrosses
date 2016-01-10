f9f41dd408c1c1ce2fbfe194460ef03b2e5cb86b
Eric Mugerwa <ericmdev@gmail.com>
2016-01-10 00:34:28 +0000

    Add src/Config/ConfigInterface.php

---
+++ src/Config/ConfigInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace NoughtsAndCrosses\Config;
+
+/** 
+ * Interface for Config.
+ *
+ * @package   noughtsandcrosses
+ */
+interface ConfigInterface
+{
+    public function get();
+    public function set($data);
+    public function parse($file);
+}
