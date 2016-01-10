11c0aedc6f70ec0517ec2c6d3271b395ac21db53
Eric Mugerwa <ericmdev@gmail.com>
2016-01-10 14:12:40 +0000

    Add test/unit/Config/Config.php

---
+++ test/unit/Config/Config.php
@@ -0,0 +1,14 @@
+<?php
+namespace NoughtsAndCrosses\Test;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Config unit test.
+ *
+ * @package noughtsandcrosses
+ * @group   specification
+ */
+class Config_UnitTest extends PHPUnit_Framework_TestCase
+{
+
+}
