e41edda07c7234362f48abd192a60767653419db
Eric Mugerwa <ericmdev@gmail.com>
2016-01-11 01:13:52 +0000

    Add src/NeuralNetwork/Layer/HiddenLayer.php

---
+++ src/NeuralNetwork/Layer/HiddenLayer.php
@@ -0,0 +1,19 @@
+<?php
+namespace NoughtsAndCrosses\NeuralNetwork\Layer;
+
+/**
+ * HiddenLayer
+ *
+ * @package noughtsandcrosses
+ */
+class HiddenLayer implements HiddenLayerInterface
+{
+    /**
+     * Constructor.
+     * 
+     */
+    public function __construct()
+    {
+        
+    }
+}
