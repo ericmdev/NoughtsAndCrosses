ebb1ff652ccd462405febeb8b8e55636ee50e242
Eric Mugerwa <ericmdev@gmail.com>
2016-01-11 00:44:52 +0000

    Add src/NeuralNetwork/Layer/Layer_AbstractInterface.php

---
+++ src/NeuralNetwork/Layer/Layer_AbstractInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace NoughtsAndCrosses\NeuralNetwork\Layer;
+
+/** 
+ * Interface for Abstract Layer.
+ *
+ * @package noughtsandcrosses
+ */
+interface Layer_AbstractInterface
+{
+	
+}
