677dd4146c960fafbd96199c74590f72a486af0e
Eric Mugerwa <ericmdev@gmail.com>
2016-01-11 00:12:35 +0000

    Add test/unit/NeuralNetwork/NeuralNetwork.php

---
+++ test/unit/NeuralNetwork/NeuralNetwork.php
@@ -0,0 +1,15 @@
+<?php
+namespace NoughtsAndCrosses\Test;
+use NoughtsAndCrosses\NeuralNetwork\NeuralNetwork;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * NeuralNetwork unit test.
+ *
+ * @package noughtsandcrosses
+ * @group   specification
+ */
+class NeuralNetwork_UnitTest extends NoughtsAndCrossesTestCase
+{
+
+}
