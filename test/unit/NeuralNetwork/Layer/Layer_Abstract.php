1bbcf49e1d8ad25d288a527c2ddcc3a8c519e130
Eric Mugerwa <ericmdev@gmail.com>
2016-01-11 01:31:34 +0000

    Add test/unit/NeuralNetwork/Layer/Layer_Abstract.php

---
+++ test/unit/NeuralNetwork/Layer/Layer_Abstract.php
@@ -0,0 +1,15 @@
+<?php
+namespace NoughtsAndCrosses\Test;
+use NoughtsAndCrosses\NeuralNetwork\Layer\Layer_Abstract;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Layer_Abstract unit test.
+ *
+ * @package noughtsandcrosses
+ * @group   specification
+ */
+class Layer_Abstract_UnitTest extends NoughtsAndCrossesTestCase
+{
+	
+}
