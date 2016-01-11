8a677c2d8ba563695b28271565e82c6ed6313297
Eric Mugerwa <ericmdev@gmail.com>
2016-01-11 00:05:00 +0000

    Add class interface

---
+++ src/Game/Player/NeuralNetwork/Neuron/Neuron.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Neuron
+ *
+ * @package   NoughtsAndCrosses
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+namespace NoughtsAndCrosses\Game\Player\NeuralNetwork\Neuron;
+
+
+/**
+ * Neuron
+ *
+ * No description.
+ */
+class Neuron implements NeuronInterface
+{
+
+    /**
+     * Constructor.
+     * 
+     */
+    public function __construct()
+    {  
+    	echo "Created neuron.";
+    }
+}
--- src/Game/Player/Player.php
+++ src/Game/Player/Player.php
@@ -16,4 +16,46 @@ class Player implements PlayerInterface
     {
         
     }
+
+    /**
+     * Returns the number property.
+     * 
+     * @return int
+     */
+    public function getNumber()
+    {
+
+    }
+
+    /**
+     * Sets the number property.
+     * 
+     * @param  int $number Player number.
+     * @return int
+     */
+    public function setNumber($number)
+    {
+
+    }
+
+    /**
+     * Returns the neuralNetwork property.
+     * 
+     * @return NeuralNetworkInterface
+     */
+    public function getNeuralNetwork()
+    {
+
+    }
+
+    /**
+     * Sets the neuralNetwork property.
+     * 
+     * @param  NeuralNetworkInterface $neuralNetwork Neural network.
+     * @return NeuralNetworkInterface
+     */
+    public function setNeuralNetwork($neuralNetwork)
+    {
+    	
+    }
 }
