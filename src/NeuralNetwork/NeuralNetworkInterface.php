0ef76ac78411ed7bbc16895bf6b822ad8c2c5157
Eric Mugerwa <ericmdev@gmail.com>
2016-01-11 00:46:50 +0000

    Add getHiddenLayer setHiddenLayer

--- src/NeuralNetwork/NeuralNetworkInterface.php
+++ src/NeuralNetwork/NeuralNetworkInterface.php
@@ -22,5 +22,20 @@ interface NeuralNetworkInterface
      * @param  InputLayerInterface $layer Input layer.
      * @return InputLayerInterface
      */
-    public function setInputLayer(InputLayerInterface $layer);	
+    public function setInputLayer(InputLayerInterface $layer);
+
+    /**
+     * Returns the hidden layer property.
+     * 
+     * @return HiddenLayerInterface
+     */
+    public function getHiddenLayer();
+
+    /**
+     * Set hidden layer.
+     * 
+     * @param  HiddenLayerInterface $layer Hidden layer.
+     * @return HiddenLayerInterface
+     */
+    public function setHiddenLayer(HiddenLayerInterface $layer);
 }
