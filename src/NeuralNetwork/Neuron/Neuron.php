1d9090785298830f11247d1eda6b2e20779255ae
Eric Mugerwa <ericmdev@gmail.com>
2016-01-11 01:15:45 +0000

    Add src/Game/Player/NeuralNetwork/Neuron/Neuron.php

---
+++ src/NeuralNetwork/Neuron/Neuron.php
@@ -0,0 +1,19 @@
+<?php
+namespace NoughtsAndCrosses\NeuralNetwork\Neuron;
+
+/**
+ * Neuron
+ *
+ * @package noughtsandcrosses
+ */
+class Neuron implements NeuronInterface
+{
+    /**
+     * Constructor.
+     * 
+     */
+    public function __construct()
+    {
+
+    }
+}
