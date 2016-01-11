f469f1e53841402bf3250aa6b2a06514aebc6135
Eric Mugerwa <ericmdev@gmail.com>
2016-01-11 00:23:25 +0000

    Add src/NeuralNetwork/Layer/InputLayer.php

---
+++ src/NeuralNetwork/Layer/InputLayer.php
@@ -0,0 +1,19 @@
+<?php
+namespace NoughtsAndCrosses\NeuralNetwork\Layer;
+
+/**
+ * InputLayer
+ *
+ * @package noughtsandcrosses
+ */
+class InputLayer implements InputLayerInterface
+{
+    /**
+     * Constructor.
+     * 
+     */
+    public function __construct()
+    {
+        
+    }
+}
