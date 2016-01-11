4bea82a0bfa660c9f0f8c3dd87c8a41fbbeef781
Eric Mugerwa <ericmdev@gmail.com>
2016-01-11 00:13:00 +0000

    Add src/NeuralNetwork/NeuralNetwork.php

---
+++ src/NeuralNetwork/NeuralNetwork.php
@@ -0,0 +1,19 @@
+<?php
+namespace NoughtsAndCrosses\NeuralNetwork;
+
+/**
+ * NeuralNetwork
+ *
+ * @package noughtsandcrosses
+ */
+class NeuralNetwork implements NeuralNetworkInterface
+{
+    /**
+     * Constructor.
+     * 
+     */
+    public function __construct()
+    {
+        
+    }
+}
