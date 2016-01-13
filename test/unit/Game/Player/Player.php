85b8581308f36735472d24c42dea031a1e43671a
Eric Mugerwa <ericmdev@gmail.com>
2016-01-13 19:07:16 +0000

    Add test/unit/Game/Player/Player.php

---
+++ test/unit/Game/Player/Player.php
@@ -0,0 +1,15 @@
+<?php
+namespace NoughtsAndCrosses\Test;
+use NoughtsAndCrosses\Player\Player;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Player unit test.
+ *
+ * @package noughtsandcrosses
+ * @group   specification
+ */
+class Player_UnitTest extends NoughtsAndCrossesTestCase
+{
+    
+}
