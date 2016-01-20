e8617adf361b9d9d222ed37864589a2fd27f94d0
Eric Mugerwa <ericmdev@gmail.com>
2016-01-20 18:41:13 +0000

    Add src/ApplicationServiceProvider.php

---
+++ src/ApplicationServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace NoughtsAndCrosses\Application;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * Service Provider for Application.
+ *
+ * @package noughtsandcrosses
+ */
+class ApplicationServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $pimple)
+    {
+        
+    }
+}
